609a4b8d40851a74a8890e80b4a5a414b3369bd9
Hessel <h.j.akkerman@student.utwente.nl>
2021-06-23 23:55:08 +0200

    some work on UI tests

--- UITests/UnitTest1.cs
+++ UITests/UnitTest1.cs
@@ -37,13 +37,42 @@ namespace UITests
         }
 
         [Test]
-        public void CanInsertDishInRow()
+        public void InsertDishTest()
         {
-            
+            IRenderedComponent<ConnectFour> gamePage = testContext.RenderComponent<ConnectFour>();
+
+
+            //get two dimensional array
+
+            //check if the size is right
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 10; y > 0; y--)
+                {
+                    //click on cell
+
+
+                    //check if dish is in row
+
+                    if(true) //this cell is empty
+                    {
+                        //get current player
+
+                        //check if this cell is not current players color color
+                    }
+                }
+            }
         }
 
         [Test]
-        public void CanClickResetGameButton()
+        public void ClickOnOccupiedCellTest()
+        { 
+        
+        }
+
+        [Test]
+        public void CanPlayFullGame()
         {
             //play game and win
 
